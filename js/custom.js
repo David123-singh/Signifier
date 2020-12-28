f92f4ce90aeb1abf3e10edcd0878ffd4a36fbbbd
David123-singh <devitsingh136@gmail.com>
2020-12-28 20:11:49 +0530

    service page add

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,13 @@ $(window).scroll(function () {
         $(".header").removeClass("whiteBg");
     }
 })
+
+var mobile_btn = document.querySelector(".mobile-close-btn");
+var menu_sec = document.querySelector(".navWrapper");
+var bg_scroll = document.querySelector("body");
+mobile_btn.addEventListener("click", function(){
+    mobile_btn.classList.toggle("open-menu"); 
+    menu_sec.classList.toggle("nav-open");
+    bg_scroll.classList.toggle("scroll-hide")
+});
+
